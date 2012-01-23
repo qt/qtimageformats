e550655e553895b0ca8cf66283933b8562030d40
Kent Hansen <kent.hansen@nokia.com>
2012-01-23 11:57:17 +0100

    Long live the qtimageformats module
    This module contains plugins for image formats that aren't part of
    the "core" (qtbase) offering, starting with mng.
    
    Task-number: QTBUG-21869
    Change-Id: I85e51ed6f84c07419b1676f9def234c36f10141f
    Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: aavit <qt_aavit@ovi.com>

---
+++ tests/benchmarks/mng/tst_qmng.cpp
@@ -0,0 +1,108 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the QtAddOn.JsonDb module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#include <QtTest/QtTest>
+#include <QtGui/QtGui>
+
+class tst_qmng: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void readImage_data();
+    void readImage();
+    void readCorruptImage_data();
+    void readCorruptImage();
+};
+
+void tst_qmng::readImage_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<QSize>("size");
+
+    QTest::newRow("animation") << QString("animation.mng") << QSize(100, 100);
+    QTest::newRow("ball") << QString("ball.mng") << QSize(32, 32);
+    QTest::newRow("dutch") << QString("dutch.mng") << QSize(352, 264);
+    QTest::newRow("fire") << QString("fire.mng") << QSize(30, 60);
+}
+
+void tst_qmng::readImage()
+{
+    QFETCH(QString, fileName);
+    QFETCH(QSize, size);
+
+    QString path = QString(":/mng/") + fileName;
+    QBENCHMARK {
+        QImageReader reader(path);
+        QVERIFY(reader.canRead());
+        QImage image = reader.read();
+        QVERIFY(!image.isNull());
+        QCOMPARE(image.size(), size);
+    }
+}
+
+void tst_qmng::readCorruptImage_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<QString>("message");
+
+    QTest::newRow("corrupt")
+            << QString("corrupt.mng")
+            << QString("MNG error 901: Application signalled I/O error; chunk IHDR; subcode 0:0");
+}
+
+void tst_qmng::readCorruptImage()
+{
+    QFETCH(QString, fileName);
+    QFETCH(QString, message);
+
+    QString path = QString(":/mng/") + fileName;
+    QBENCHMARK {
+        QImageReader reader(path);
+        if (!message.isEmpty())
+            QTest::ignoreMessage(QtWarningMsg, message.toLatin1());
+        QVERIFY(reader.canRead());
+        QImage image = reader.read();
+        QVERIFY(image.isNull());
+    }
+}
+
+QTEST_MAIN(tst_qmng)
+#include "tst_qmng.moc"
