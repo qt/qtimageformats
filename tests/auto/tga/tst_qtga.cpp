e1da99d51ec124db091c5cac8a513770e72814a7
Toby Tomkins <toby.tomkins@nokia.com>
2012-02-02 11:19:43 +1000

    Move tga support from Qt3d to QtImageFormats.
    Rev-By: Samuel Rodal
    Fix-for: QTBUG-21955
    
    Part of fix for https://bugreports.qt.nokia.com/browse/QTBUG-21955
    
    Having tga plugin inside a Qt add-on causes packaging problems.
    
    There have been many queries over the years for tga support even tho it
    is a niche image format. It is particularly useful for OpenGL work as a
    texture format since it is a blittable bitmap, but it is very
    innefficient compared to png or jpg.
    
    For this reason only read support is added, and this is documented.
    Also add one unit test.
    
    (based on cherry picked from Qt 4.8 commit 951a997893407b0a26f21fe0acec4701fa3d4279)
    
    Adds code removed from Qt3D.
    http://codereview.qt-project.org/#change,13898
    
    Conflicts:
    
    tests/auto/qimagereader/qimagereader.pro
    tests/auto/qimagereader/tst_qimagereader.cpp
    
    Change-Id: Ia1b7420dd3461cb1d2f1d857d11444a0a533de43

---
+++ tests/auto/tga/tst_qtga.cpp
@@ -0,0 +1,76 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the TGA autotests in the Qt ImageFormats module.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtTest/QtTest>
+#include <QtGui/QtGui>
+
+class tst_qtga: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void readImage_data();
+    void readImage();
+};
+
+void tst_qtga::readImage_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<QSize>("size");
+
+    QTest::newRow("test-flag") << QString("test-flag.tga") << QSize(400, 400);
+}
+
+void tst_qtga::readImage()
+{
+    QFETCH(QString, fileName);
+    QFETCH(QSize, size);
+
+    QString path = QString(":/tga/") + fileName;
+    QImageReader reader(path);
+    QVERIFY(reader.canRead());
+    QImage image = reader.read();
+    QVERIFY(!image.isNull());
+    QCOMPARE(image.size(), size);
+}
+
+QTEST_MAIN(tst_qtga)
+#include "tst_qtga.moc"
