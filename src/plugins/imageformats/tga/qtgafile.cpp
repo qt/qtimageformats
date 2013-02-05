6447c6da1100a4734c9cf8aa74678a10b384aa31
Friedemann Kleint <Friedemann.Kleint@digia.com>
2013-02-05 16:11:19 +0100

    Fix spelling errors in QTgaFile messages.
    Change-Id: I967f20698d161a52365c9bfea68ca0c7596b998b

--- src/plugins/imageformats/tga/qtgafile.cpp
+++ src/plugins/imageformats/tga/qtgafile.cpp
@@ -149,7 +149,7 @@ QTgaFile::QTgaFile(QIODevice *device)
     int bytes = device->read((char*)mHeader, HeaderSize);
     if (bytes != HeaderSize)
     {
-        mErrorMessage = QObject::tr("Image mHeader read failed");
+        mErrorMessage = QObject::tr("Image header read failed");
         return;
     }
     if (mHeader[ImageType] != 2)
@@ -162,7 +162,7 @@ QTgaFile::QTgaFile(QIODevice *device)
     bool validDepth = (bitsPerPixel == 16 || bitsPerPixel == 24 || bitsPerPixel == 32);
     if (!validDepth)
     {
-        mErrorMessage = QObject::tr("Image dpeth not valid");
+        mErrorMessage = QObject::tr("Image depth not valid");
     }
     int curPos = mDevice->pos();
     int fileBytes = mDevice->size();
