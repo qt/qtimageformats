e970e4a394c4e2f9d9fc50a31657cbddcb64d501
Alexandru Croitor <alexandru.croitor@qt.io>
2019-06-05 22:45:38 +0200

    Fix qtimageformats building
    Some feature conditions were incorrect.
    Added forgotten FindWrapWebP.cmake file.
    Protect call to qt_find_package ZLIB.
    
    Change-Id: I033d38e4f69e94f7b72346b49bf6de4b2dbde8e0
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/imageformats/configure.cmake
+++ src/imageformats/configure.cmake
@@ -38,7 +38,7 @@ qt_feature("mng" PRIVATE
 )
 qt_feature("tiff" PRIVATE
     LABEL "TIFF"
-    CONDITION QT_FEATURE_imageformatplugin
+    CONDITION QT_FEATURE_imageformatplugin AND TIFF_FOUND
     DISABLE INPUT_tiff STREQUAL 'no'
 )
 qt_feature("system_tiff" PRIVATE
@@ -49,7 +49,7 @@ qt_feature("system_tiff" PRIVATE
 )
 qt_feature("webp" PRIVATE
     LABEL "WEBP"
-    CONDITION QT_FEATURE_imageformatplugin
+    CONDITION QT_FEATURE_imageformatplugin AND WrapWebP_FOUND
     DISABLE INPUT_webp STREQUAL 'no'
 )
 qt_feature("system_webp" PRIVATE
