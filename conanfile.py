65b9d881ad90678457a0d147a6453c50400a9855
Iikka Eklund <iikka.eklund@qt.io>
2021-03-04 09:33:56 +0200

    Add "scm" attribute to conanfile.py
    We want to match the commit ID along with the
    RREV (recipe revision). This way we can pin exact Conan packages
    to known sha1/tag which makes releasing easier.
    
    https://docs.conan.io/en/latest/reference/conanfile/attributes.html#revision-mode
    
    Task-number: QTQAINFRA-4325
    Change-Id: I0e34081429c545b73f549a2aea739dcaee98c11e
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit e2bed44b317951494d30feb2910a05ba87ea1b63)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- conanfile.py
+++ conanfile.py
@@ -48,6 +48,7 @@ class QtImageFormats(ConanFile):
     default_options = {"shared": False,
                        "qt6": None}
     exports_sources = "*", "!conan*.*"
+    revision_mode = "scm"  # use commit ID as the RREV (recipe revision)
 
     def source(self):
         # sources are installed next to recipe, no need to clone etc. sources here
